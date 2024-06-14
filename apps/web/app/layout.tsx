308918b8aab0e3d21c9ae933cea79ce7ea514dfb
lucas <lucasabazemore@gmail.com>
2024-06-13 20:15:24 -0500

    add clarity

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import { Providers } from './providers';
 import { poppins } from '../components/fonts';
 
@@ -18,6 +19,15 @@ export default function RootLayout({
 }): JSX.Element {
   return (
     <html lang="en">
+      <Script id="clarity">
+        {`
+					(function(c,l,a,r,i,t,y){
+							c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+							t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+							y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+					})(window, document, "clarity", "script", "mrklr8g3bl");
+				`}
+      </Script>
       <body
         className={poppins.className}
         style={{ backgroundColor: '#FFFBF8' }}
