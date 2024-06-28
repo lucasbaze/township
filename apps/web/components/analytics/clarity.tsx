d11845218fdb23562b34a9d63618aa11b4493ce7
lucas <lucasabazemore@gmail.com>
2024-06-27 20:16:37 -0500

    fix clarity load

--- apps/web/components/analytics/clarity.tsx
+++ apps/web/components/analytics/clarity.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import Script from 'next/script';
 
 export const Clarity = () => (
-  <Script id="clarity" strategy="afterInteractive">
+  <Script id="clarity-script" strategy="afterInteractive">
     {`
-					(function(c,l,a,r,i,t,y){
-							c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-							t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-							y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-					})(window, document, "clarity", "script", ${process.env.NEXT_PUBLIC_CLARITY_ID});
-				`}
+			(function(c,l,a,r,i,t,y){
+					c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+					t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+					y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+			})(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_ID}");
+		`}
   </Script>
 );
