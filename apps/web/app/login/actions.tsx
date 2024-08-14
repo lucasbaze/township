cd22d162f61714018a551038eae747f971c96fe4
lucas <lucasabazemore@gmail.com>
2024-08-14 14:42:55 -0500

    login cleanup

--- apps/web/app/login/actions.tsx
+++ apps/web/app/login/actions.tsx
@@ -22,25 +22,5 @@ export async function login(formData: FormData) {
   }
 
   revalidatePath('/', 'layout');
-  redirect('/private');
-}
-
-export async function signup(formData: FormData) {
-  const supabase = createClient();
-
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
-    email: formData.get('email') as string,
-    password: formData.get('password') as string,
-  };
-
-  const { error } = await supabase.auth.signUp(data);
-  console.log('Error: ', error);
-  if (error) {
-    redirect('/error');
-  }
-
-  revalidatePath('/', 'layout');
-  redirect('/');
+  redirect('/dashboard');
 }
--- apps/web/app/login/page.tsx
+++ apps/web/app/login/page.tsx
@@ -1,14 +1,60 @@
-import { login, signup } from './actions';
+import React from 'react';
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Box,
+  Heading,
+  Text,
+} from '@chakra-ui/react';
+import { login } from './actions';
 
-export default function LoginPage() {
+const LoginPage: React.FC = () => {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
+    <Box
+      maxW="md"
+      mx="auto"
+      my={16}
+      p={8}
+      borderWidth={1}
+      borderRadius="lg"
+      boxShadow="lg"
+    >
+      <Heading as="h1" size="lg" textAlign="center" mb={6}>
+        Dashboard Login
+      </Heading>
+
+      <form>
+        <FormControl id="email" mb={4} isRequired>
+          <FormLabel>Email:</FormLabel>
+          <Input name="email" type="email" placeholder="Enter your email" />
+        </FormControl>
+
+        <FormControl id="password" mb={6} isRequired>
+          <FormLabel>Password:</FormLabel>
+          <Input
+            name="password"
+            type="password"
+            placeholder="Enter your password"
+          />
+        </FormControl>
+
+        <Box display="flex" alignItems="center" mb={6}>
+          <Button type="submit" formAction={login} variant="primary">
+            Log in
+          </Button>
+        </Box>
+
+        <Text fontSize="sm" textAlign="center" color="gray.500" mb={4}>
+          Signups are not available freely. You must have an account.
+        </Text>
+        <Text fontSize="sm" textAlign="center" color="gray.500">
+          For password reset, please email us.
+        </Text>
+      </form>
+    </Box>
   );
-}
+};
+
+export default LoginPage;
