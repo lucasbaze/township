2fdfe43fd0b0330c9c6369857f2757351b9834e5
lucas <lucasabazemore@gmail.com>
2024-12-09 10:04:45 -0600

    add public endpoint

--- apps/web/app/api/logs/route.ts
+++ apps/web/app/api/logs/route.ts
@@ -1,14 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { createClient } from '../../../utils/supabase/server';
 
-// Set up CORS to allow any domain
-export const config = {
-  api: {
-    bodyParser: true,
-    externalResolver: true,
-  },
-};
-
 export async function POST(req: NextRequest) {
   // Allow any domain to access this endpoint
   const headers = new Headers();
@@ -21,21 +13,28 @@ export async function POST(req: NextRequest) {
   }
 
   try {
+    // Log the incoming request
+    console.log('Incoming request:', req);
+
     const body = await req.text(); // Accept a string as the body
+    console.log('Incoming Body:', body);
     const supabase = createClient();
 
     const { error } = await supabase.from('logs').insert({ log: body });
 
     if (error) {
       return NextResponse.json(
-        { error: error.message },
+        { error: { message: error.message, code: 500 } },
         { status: 500, headers },
       );
     }
 
     return NextResponse.json({ success: true }, { status: 200, headers });
   } catch (err) {
-    console.log(err);
-    return NextResponse.json({ error: `${err}` }, { status: 500, headers });
+    console.log('Error:', err);
+    return NextResponse.json(
+      { error: { message: `${err}`, code: 500 } },
+      { status: 500, headers },
+    );
   }
 }
